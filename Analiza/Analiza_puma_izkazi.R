137a6fdfc7703bca0939036ba60b8321dc0eb814
Zala Stopar <zala.stopar1@gmail.com>
2020-11-28 22:32:51 +0100

    Uvoz IGK za Puma

--- Analiza/Analiza_puma_izkazi.R
+++ Analiza/Analiza_puma_izkazi.R
@@ -1,13 +1,12 @@
 # Analiza bilance
 bilanca_puma_analiza <- cbind(bilanca_puma[3:25,2]/4918469,
                                    bilanca_puma[3:25,3]/3672245.850, bilanca_puma[3:25,4]/3422561.1240,
-                                   bilanca_puma[3:25,5]/2914690.6928)
+                                   bilanca_puma[3:25,5]/2914690.6928, bilanca_puma[3:25,6]/2852720.4446)
 bilanca_puma_analiza <- round(bilanca_puma_analiza, 3)
 bilanca_puma_analiza <- cbind(bilanca_puma[3:25,1], bilanca_puma_analiza)
 
 
 # Analiza IPI
-IPI_puma_analiza <- cbind(IPI_puma[3:28, 2]/IPI_puma[3:28,5], IPI_puma[3:28, 3]/IPI_puma[3:28,5], 
-                          IPI_puma[3:28, 4]/IPI_puma[3:28,5], IPI_puma[3:28, 5]/IPI_puma[3:28,5])
-IPI_puma_analiza <- cbind(IPI_puma[3:28, 1], round(IPI_puma_analiza, 2) * 100)
+IPI_puma_analiza <- cbind(IPI_puma[3:14, 1], round(IPI_puma[,2:6]/IPI_puma$"2015" * 100)[3:14,])
+
 
--- Uvoz/uvoz_izkazov.R
+++ Uvoz/uvoz_izkazov.R
@@ -11,26 +11,30 @@ IDT_nike <- read_excel("Data-Nike/IDT_nike.xlsx", col_names = c("kategorija", 20
 
 # PUMA
 
-bilanca_puma <- read_excel("Data-Puma/Bilanca_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+bilanca_puma <- read_excel("Data-Puma/Bilanca_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                            na = "n.a.", skip = 1)
+row.names(bilanca_puma) <- bilanca_puma$kategorija
 
-IPI_puma <- read_excel("Data-Puma/IPI_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+IPI_puma <- read_excel("Data-Puma/IPI_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                        na = "n.a.", skip = 1)
 
-IDT_puma <- read_excel("Data-Puma/IDT_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+IDT_puma <- read_excel("Data-Puma/IDT_puma.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                        na = "n.a.", skip = 1)
 
-#manjka IGK
+IGK_puma <- read_excel("Data-Puma/IGK_puma.xlsx")
+
+
+
 
 # ADIDAS
 
-bilanca_adidas <- read_excel("Data-Adidas/bilanca_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+bilanca_adidas <- read_excel("Data-Adidas/bilanca_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                              na = "n.a.", skip = 1)
 
-IPI_adidas <- read_excel("Data-Adidas/IPI_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+IPI_adidas <- read_excel("Data-Adidas/IPI_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                          na = "n.a.", skip = 1)
 
-IDT_adiadas <- read_excel("Data-Adidas/IDT_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016), 
+IDT_adiadas <- read_excel("Data-Adidas/IDT_adidas.xlsx", col_names = c("kategorija", 2019, 2018, 2017, 2016, 2015), 
                           na = "n.a.", skip = 1)
 
 #manjka IGK
